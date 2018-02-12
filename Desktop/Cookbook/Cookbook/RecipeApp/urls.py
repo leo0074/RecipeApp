ec04515ed9416d37e9c5f9538d65c0702a9acaca
Leo Pälkiö <leo.palkio@anlashok.net>
2018-02-12 13:10:15 +0200

    Page with login requirement

--- Desktop/Cookbook/Cookbook/RecipeApp/urls.py
+++ Desktop/Cookbook/Cookbook/RecipeApp/urls.py
@@ -1,7 +1,8 @@
 from django.urls import include, path
 from django.contrib import admin
-from RecipeApp.views import login_view
+from RecipeApp.views import login_view, recipes_view
 urlpatterns = [
     path('login', login_view.login, name='login'),
+	path('recipes', recipes_view.recipes, name='recipes'),
     path('admin/', admin.site.urls),
 ]
--- Desktop/Cookbook/Cookbook/RecipeApp/views/login_view.py
+++ Desktop/Cookbook/Cookbook/RecipeApp/views/login_view.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 
 
 def login(request):
-    return HttpResponse("This iwll be the login page")
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			#login(request, user)
+			return HttpResponse("Login successful!")
+		else:
+			return HttpResponse("Login failed")
+	if request.method == 'GET':
+		return render(request, 'RecipeApp/login.html')
+	
