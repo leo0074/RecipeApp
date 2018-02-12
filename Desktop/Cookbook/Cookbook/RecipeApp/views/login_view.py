ae5ef84dd086b847fc834f850fc3b578814c619b
Leo Pälkiö <leo.palkio@anlashok.net>
2018-02-12 14:15:28 +0200

    Login page to redirect error back to page

--- Desktop/Cookbook/Cookbook/RecipeApp/views/login_view.py
+++ Desktop/Cookbook/Cookbook/RecipeApp/views/login_view.py
@@ -1,18 +1,20 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 
 
-def login(request):
+def login_page(request):
+
 	if request.method == 'POST':
 		username = request.POST['username']
 		password = request.POST['password']
 		user = authenticate(request, username=username, password=password)
 		if user is not None:
-			#login(request, user)
-			return HttpResponse("Login successful!")
+			login(request, user)
+			return HttpResponse('Login successful!')
 		else:
-			return HttpResponse("Login failed")
+			return render(request, 'RecipeApp/login.html', {'username' : username, 'message' : 'Invalid username or password'})
 	if request.method == 'GET':
 		return render(request, 'RecipeApp/login.html')
 	
