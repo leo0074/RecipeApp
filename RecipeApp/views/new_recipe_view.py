3309fd9aa39e21efaaaa1862a6cfad47fafc7f81
Leo Pälkiö <leo.palkio@anlashok.net>
2018-02-21 15:52:48 +0200

    multple ingredients in the page

--- RecipeApp/views/new_recipe_view.py
+++ RecipeApp/views/new_recipe_view.py
@@ -8,18 +8,10 @@ from django.contrib.auth.decorators import login_required
 def new_recipe(request):
 
 	if request.method == 'POST':
-		username = request.POST['username']
-		password = request.POST['password']
-		user = authenticate(request, username=username, password=password)
-		if user is not None:
-			login(request, user)
-			next = request.POST['next']
-			if next != '':
-				return render(request, next[1:]+'.html')
-			else:
-				return render(request, 'RecipeApp/login_success.html')
-		else:
-			return render(request, 'RecipeApp/login.html', {'username' : username, 'message' : 'Invalid username or password', 'next' : request.POST['next']})
+		username = request.user.username
+		ingredients = []
+		recipe = request.POST['recipe']
+		return render(request, 'RecipeApp/new_recipe.html')
 	
 	if request.method == 'GET':
 
