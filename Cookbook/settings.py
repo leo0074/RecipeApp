f16a324ebd550c3f7b26666cf5cb0b14765f8ef1
Leo Pälkiö <leo.palkio@anlashok.net>
2018-03-02 11:13:48 +0200

    Heroku deplayment fixes

--- Cookbook/settings.py
+++ Cookbook/settings.py
@@ -92,12 +92,12 @@ DATABASES = {
     }
 }
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-#}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
