3a9af4c587da9b09ad54dff7b1eb8c89669890e7
Leo Pälkiö <leo.palkio@anlashok.net>
2018-03-02 11:05:13 +0200

    Changes for Heroku deployment

--- Cookbook/wsgi.py
+++ Cookbook/wsgi.py
@@ -8,13 +8,8 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
 import os
-from whitenoise.django import DjangoWhiteNoise
 from django.core.wsgi import get_wsgi_application
-import sys
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-sys.path.append(BASE_DIR)
-os.environ['DJANGO_SETTINGS_MODULE'] = 'Cookbook.settings'
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Cookbook.settings")
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
